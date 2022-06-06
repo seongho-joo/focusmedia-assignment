00386c0207c8ed2fbabacb17374d9a8057cbab88
seongho-joo <hazzyseong@gmail.com>
2022-06-06 21:35:37 +0900

    chore: 프로젝트 생성

---
+++ src/test/java/focusmedia/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package focusmedia.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
