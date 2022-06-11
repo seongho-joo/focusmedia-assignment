216c31f34a7fb95571e989d7768a8306c691ebf8
seongho-joo <hazzyseong@gmail.com>
2022-06-11 20:41:30 +0900

    feat: Swagger DTO field 설명 추가

--- src/main/java/focusmedia/assignment/client/dto/ClientFindResponse.java
+++ src/main/java/focusmedia/assignment/client/dto/ClientFindResponse.java
@@ -1,6 +1,8 @@
 package focusmedia.assignment.client.dto;
 
 import focusmedia.assignment.client.domain.Client;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,16 +13,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ClientFindResponse {
 
+    @ApiModelProperty(value = "식별자")
     private Long id;
 
+    @ApiModelProperty(example = "홍길동", value = "고객 이름")
     private String name;
 
+    @ApiModelProperty(example = "hong@email.com", value = "고객 이메일")
     private String email;
 
+    @ApiModelProperty(example = "82", value = "국가 코드")
     private Integer countryCode;
 
+    @ApiModelProperty(example = "01011112222", value = "고객 연락처")
     private String phoneNumber;
 
+    @ApiModelProperty(example = "true", value = "개인정보 수집 동의 여부")
     private Boolean personalInformationAgreement;
 
     public static ClientFindResponse from(Client client) {
